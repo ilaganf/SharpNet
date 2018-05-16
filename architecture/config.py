a9667bf2a09236ce234b7cf780fc78e10ddace96
Vik Pattabi <vikpattabi@DN51uc9q.SUNet>
2018-05-15 22:10:45 -0700

    Implemented config.py

--- architecture/config.py
+++ architecture/config.py
@@ -5,7 +5,25 @@ File for defining object that is responsible
 for holding hyperparameters for the model
 '''
 
+import json
+
 class Config():
+
+    def load_params(self, filename):
+        with open(filename) as f:
+            data = json.load(f)
+            for key in data.keys():
+                self.__dict__[key] = data[key]
+
+    def write_params(self, filename):
+        # Create dict...
+        dict_to_write = {}
+        for key in self.__dict__.keys():
+            dict_to_write[key] = self.__dict__[key]
+
+        with open(filename, 'w') as f:
+            json.dump(dict_to_write, f)
+
 
     def __init__(self, *args):
         pass
