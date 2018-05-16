41ecddc3fe4dac6bc31f4888ea27b283e3058282
ilaganf <ilaganf@stanford.edu>
2018-05-15 21:55:23 -0700

    starting to create training loop

--- architecture/model.py
+++ architecture/model.py
@@ -16,7 +16,17 @@ class EnhanceNet():
     so they can be readily accessed for training/testing
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, inputs, config, is_training=True):
 
-    
+        # Iterator over dataset object
+        self.inputs = inputs[0]
+        self.labels = inputs[1]
+
+        # Config object that holds hyperparameters
+        self.config = config
+
+
+    def add_training_op(self, loss):
+        optimizer = tf.train.AdamOptimizer(self.config.learning_rate)
+        global_step = tf.train.get_or_create_global_step()
+        return optimizer.minimize(loss, global_step=global_step, scope='model_prediction')
