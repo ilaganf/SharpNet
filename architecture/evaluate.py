145236c822a898bcd4206d563ae1ba38cd272f7c
ilaganf <ilaganf@stanford.edu>
2018-05-16 23:54:41 -0700

    small bug fixes

--- architecture/evaluate.py
+++ architecture/evaluate.py
@@ -7,9 +7,6 @@ through the test set.
 import os
 
 import tensorflow as tf
-import scipy
-import numpy as np
-from input import input_op
 
 
 def evaluate(model, params):
@@ -23,7 +20,7 @@ def evaluate(model, params):
         total_loss = 0.0
         total_ssim = 0.0
         total_psnr = 0.0
-        for i in range(num_runs)
+        for i in range(num_runs):
             mse, ssim, psnr, pred = sess.run([model.loss_op, model.ssim_op, 
                                               model.psnr_op, model.prediction_op])
             total_loss += mse
@@ -31,17 +28,3 @@ def evaluate(model, params):
             total_psnr += psnr
 
         return total_loss/num_runs, total_ssim/num_runs, total_psnr/num_runs
-
-
-def main():
-    tf.set_random_seed(12345)
-    param = config.Config("baseline")
-
-    test_files = [os.path.join(config.TEST_DIR, f) for f in os.listdir(config.TEST_DIR)
-                  if f.endswith('.jpg') or f.endswith('.png')]
-
-    loss, ssim, psnr = evaluate(param, test_files)
-
-
-if __name__=="__main__":
-    main()
